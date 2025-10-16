e2707da50748634c389c433183ab565f341475af
konfucius <90789040+ggnndd@users.noreply.github.com>
2025-10-16 20:45:13 +0500

    Практики
    Практики "Гистограмма" и "Тепловая карта" из раздела массивы

---
+++ HeatmapTask.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var days = GenerateDayLabels();
+            var months = GenerateMonthLabels();
+            var birthsData = CalculateBirthsData(names);
+
+            return new HeatmapData(
+                "Тепловая карта рождаемости", 
+                birthsData, 
+                days, 
+                months);
+        }
+
+        private static string[] GenerateDayLabels()
+        {
+            var days = new string[30];
+            for (int i = 0; i < 30; i++)
+                days[i] = (i + 2).ToString();
+            return days;
+        }
+
+        private static string[] GenerateMonthLabels()
+        {
+            var months = new string[12];
+            for (int i = 0; i < 12; i++)
+                months[i] = (i + 1).ToString();
+            return months;
+        }
+
+        private static double[,] CalculateBirthsData(NameData[] names)
+        {
+            var birthsData = new double[30, 12];
+
+            foreach (var person in names)
+            {
+                if (ShouldSkipPerson(person))
+                    continue;
+
+                var (dayIndex, monthIndex) = GetDateIndices(person.BirthDate);
+                birthsData[dayIndex, monthIndex]++;
+            }
+
+            return birthsData;
+        }
+
+        private static bool ShouldSkipPerson(NameData person)
+        {
+            return person.BirthDate.Day == 1;
+        }
+
+        private static (int dayIndex, int monthIndex) GetDateIndices(DateTime birthDate)
+        {
+            int dayIndex = birthDate.Day - 2;
+            int monthIndex = birthDate.Month - 1;
+            return (dayIndex, monthIndex);
+        }
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,29 @@
+﻿namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var days = new string[31];
+        for (int i = 0; i < 31; i++)
+            days[i] = (i + 1).ToString();
+
+        var birthsPerDay = new double[31];
+        
+        foreach (var person in names)
+        {
+            if (person.BirthDate.Day == 1)
+                continue;
+            
+            if (person.Name.ToLower() == name.ToLower())
+            {
+                birthsPerDay[person.BirthDate.Day - 1]++;
+            }
+        }
+
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", 
+            days, 
+            birthsPerDay);
+    }
+}
